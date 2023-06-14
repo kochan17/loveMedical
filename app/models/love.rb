9c6ef354ea5d0fa8763c8499f740f44c1a45cc28
Kota <i7811832616@gmail.com>
2023-06-14 20:46:22 +0900

    コントローラー作成

--- app/models/love.rb
+++ app/models/love.rb
@@ -1,2 +1,9 @@
 class Love < ApplicationRecord
+  before_save :create_partner_info_from_user_info
+
+  private
+
+  def create_partner_info_from_user_info
+    self.ideal_partner = OpenAi.generate_partner_info(self.personality, self.occupation, self.favorite_food, self.birthday, self.background)
+  end
 end
