9782ff14031075bcc8c22956f4495b203d4af001
Kota <i7811832616@gmail.com>
2023-06-16 20:47:25 +0900

    role"system"と"user"に分別

--- app/models/open_ai.rb
+++ app/models/open_ai.rb
@@ -7,7 +7,8 @@ class OpenAi
       parameters: {
         model: 'gpt-3.5-turbo',
         messages: [
-          { role: 'user', content: "あなたはマッチングAIです。提供されたユーザープロファイルに基づいて、'算命学'や'タロット占い'のロジック、恋愛心理学の理論を通して、その理想的なパートナーの説明を生成してください。ユーザーの性格は#{personality}で、職業は#{occupation}、好きな食べ物は#{favorite_food}、誕生日は#{birthday}、経歴は#{background}です。" }
+          { role: 'system', content: "あなたはマッチングAIです。提供されたユーザープロファイルに基づいて、'算命学'や'タロット占い'のロジック、恋愛心理学の理論を通して、その理想的なパートナーの説明を生成してください。" },
+          { role: 'user', content: "ユーザーの性格は#{personality}で、職業は#{occupation}、好きな食べ物は#{favorite_food}、誕生日は#{birthday}、経歴は#{background}です。" }
         ]
       }
     )
